28444d6db54763003939eec00d2ab334774a0f02
Subramanyam S <shastri9999@gmail.com>
2017-01-02 15:05:39 +0530

    Adding Jobs Page and outline for it

--- src/components/JobsPage.js
+++ src/components/JobsPage.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router';
+import '../styles/jobspage.scss';
 
 const JobsPage = () => {
   return (
-    <div className="jobs">
-      Jobs Page
+    <div className="jobs-container">
+      <div className="navbar">
+        <div className="logo">
+          <Link to="/">
+            <img src={require('../images/logo.png')} />
+          </Link>
+        </div>
+        <div className="search-area">
+          <img src={require('../images/search-icon.png')} />
+          <input placeholder="Search by Position, Restruarant, Location" />
+        </div>
+        <div className="avatar-area">
+          <img src={require('../images/bell.png')} className="bell" />
+          <div className="user">
+            Sebastian Wussler
+          </div>
+          <img src="https://s30.postimg.org/jq2v3j0jl/userlogo.jpg" className="avatar"/>
+        </div>
+      </div>
     </div>
   );
 };
