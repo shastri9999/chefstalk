987f1da5677658710cb68eb941a5796d99bc1a78
Subramanyam S <shastri9999@gmail.com>
2017-01-27 13:03:34 +0530

    Adding award functionality

--- src/components/AwardPage.js
+++ src/components/AwardPage.js
@@ -3,6 +3,7 @@ import {go, push} from 'react-router-redux';
 import {connect} from 'react-redux';
 import AddItemButton from './AddItemButton';
 import Select from 'react-select';
+import {addProfileAward} from '../reducers/actions';
 
 class AddAwardComponent extends React.Component {
     constructor(props){
@@ -23,6 +24,7 @@ class AddAwardComponent extends React.Component {
       this.state = {
         years,
         award,
+        error: "",
       };
 
       this.onYearChange = this.onYearChange.bind(this);
@@ -50,8 +52,9 @@ class AddAwardComponent extends React.Component {
       });
     }
 
+
     render(){
-      const {onClick} = this.props;
+      const {onSave, onCancel} = this.props;
       const {award} =  this.state;
       return (<div className="add-award">
         <div className="input-container">
@@ -86,15 +89,18 @@ class AddAwardComponent extends React.Component {
           </div>
         </div>
         <div className="buttons-container">
-          <button className="save" onClick={onClick}>Save</button>
-          <button className="cancel" onClick={onClick}>Cancel</button>
+          <button className="save"
+                  onClick={()=>{onSave(award);}}>Save</button>
+          <button className="cancel"
+                  onClick={onCancel}>Cancel</button>
         </div>
       </div>);
     }
 }
 
 AddAwardComponent.propTypes = {
-  onClick: PropTypes.func,
+  onSave: PropTypes.func,
+  onCancel: PropTypes.func,
   award: PropTypes.object,
 };
 
@@ -103,8 +109,14 @@ class AwardPage extends React.Component {
     super(props);
     this.state = {
       state: "initial",
+      editingIndex: -1,
     };
     this.moveState = this.moveState.bind(this);
+    this.createAward = this.createAward.bind(this);
+  }
+
+  createAward(award){
+      this.props.addProfileAward(award);
   }
 
   moveState(state){
@@ -117,11 +129,15 @@ class AwardPage extends React.Component {
 
   render(){
     const {state} =  this.state;
+    const {awards} = this.props;
+
     return (<div className="award-page">
       <h3>Award</h3>
-      {state == "initial" ? <AddItemButton text="Add Award"
+      {(state == "initial" && !awards.length) ? <AddItemButton text="Add Award"
                                          onClick={this.moveState("adding")}/> : null }
-      {state == "adding" ? <AddAwardComponent onClick={this.moveState("initial")}/> : null}
+      {(state == "initial" && awards.length) ? <div>Listing {awards.length} awards</div> : null}
+      {state == "adding" ? <AddAwardComponent onCancel={this.moveState("initial")}
+                                              onSave={this.createAward}/> : null}
       {state == "initial" ? <div className="controls">
         <button className="previous" onClick={this.props.goBack}>Back</button>
         <button className="next" onClick={this.props.next}>Next</button>
@@ -133,7 +149,16 @@ class AwardPage extends React.Component {
 AwardPage.propTypes = {
   goBack: PropTypes.func,
   next: PropTypes.func,
+  awards: PropTypes.array,
+  addProfileAward: PropTypes.func,
 };
+
+const mapStateToProps = ({activeProfile}) =>{
+  return {
+    awards: activeProfile.awards,
+  };
+};
+
 
 const mapDispatchToProps = (dispatch)=>{
   return {
@@ -143,7 +168,10 @@ const mapDispatchToProps = (dispatch)=>{
     next: ()=>{
       dispatch(push('/welcome/education'));
     },
+    addProfileAward: (award)=>{
+      dispatch(addProfileAward(award));
+    },
   };
 };
 
-export default connect(null, mapDispatchToProps)(AwardPage);
+export default connect(mapStateToProps, mapDispatchToProps)(AwardPage);
