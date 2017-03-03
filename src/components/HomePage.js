13dc8eb72757721c1be837d3215e65824c39f2e7
Subramanyam S <shastri9999@gmail.com>
2017-03-03 22:29:44 +0530

    Making home page text correct

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -33,17 +33,17 @@ const HomePage = () => {
       </div>
       <SearchBar />
       <div className="create-profile">
-        <h2>Create Your Profile</h2>
+        <h2>How it Works</h2>
         <Slider className="steps" {...settings}>
           <div className="step">
             <div className="image-container">
                 <img src={require('../images/trophy.png')} />
             </div>
             <h3>
-              Establish a presence
+              Showcase Your Talent
             </h3>
             <p>
-            You’ll fill out a description, take and upload photos, and pick a price. Your listing helps guests get a sense of what your place is like.
+              Establish an online presence with a resume that makes you stand apart from the rest.
             </p>
           </div>
           <div className="step">
@@ -51,18 +51,22 @@ const HomePage = () => {
                 <img src={require('../images/future.png')} />
             </div>
             <h3>
-              Fuel your career
+              Drive Matches
             </h3>
-            <p>You set the availability and house rules for your listing. Host controls and calendar settings can help make hosting easier.</p>
+            <p>
+              Customize your profile with your experience and skill set. Drive more matches and get hired faster
+            </p>
           </div>
           <div className="step">
             <div className="image-container">
                 <img src={require('../images/globe.png')} />
             </div>
             <h3>
-              Learn and inspire
+              Grow Your Network
             </h3>
-            <p>From getting your home ready and choosing a price, to understanding your responsibilities under local laws — we’ve got tools and resources for you.</p>
+            <p>
+              Learn from other industry leaders and share your unique experiences on social.
+            </p>
           </div>
         </Slider>
       </div>
@@ -78,20 +82,21 @@ const HomePage = () => {
       <div className="why-chefstalk">
         <h2>Why to use ChefsTalk</h2>
         <div className="reasons">
+
           <div>
             <div className="step-number">01</div>
-            <h3>Establish a presence</h3>
-            <p>From saving for home repairs to taking a dream trip, use the extra income to fund your passions.</p>
+            <h3>Reach</h3>
+            <p>Expand your reach by posting your resume to the leading job board for culinary professionals. Learn about new opportunities, and get hired faster.</p>
           </div>
           <div>
           <div className="step-number">02</div>
-            <h3>Fuel your career</h3>
-            <p>Get tips and tools and connect with hosts like you from around the globe.</p>
+            <h3>Connect</h3>
+            <p>{"ChefsTalk is bringing the culinary community together. It's a place where culinary professionals and industry leaders from across the globe can connect and collaborate."}</p>
           </div>
           <div>
             <div className="step-number">03</div>
-            <h3>Learn and inspire</h3>
-            <p>You set your price and decide when you want to host and how often.</p>
+            <h3>Grow</h3>
+            <p>Grow your career with opportunities you find on ChefsTalk. Finding your dream job is easier than ever before. Find the role you were meant for.</p>
           </div>
         </div>
         <div className="signup">
