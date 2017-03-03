f9520d3a6a1f244cb1ab66df6730b2080948e199
Subramanyam S <shastri9999@gmail.com>
2017-03-03 20:35:35 +0530

    Show logged in state on home page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,33 @@
 import React, {PropTypes} from 'react';
 import { Link, IndexLink } from 'react-router';
 import {connect} from 'react-redux';
+import AvatarArea from './avatar.js';
+
 
 import '../styles/header.scss';
 import '../styles/header-responsive.scss';
 
-const Header = ({loggedIn}) => {
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    const {loggedIn} = this.props;
     return (
       <nav className="header">
         <IndexLink to="/" className="logo">
           <img src={require('../images/logo.png')} />
           <span className="full">Chefstalk</span>
         </IndexLink>
-        {loggedIn ? null:<div className="links">
+        {loggedIn ? <AvatarArea /> :<div className="links">
           <Link to="/restaurant" className="normal">Are you a business?</Link>
           <Link to="/login" className="login">Login</Link>
           <Link to="/signup" className="signup">Sign Up</Link>
         </div>}
       </nav>
     );
-};
+  }
+}
 
 Header.propTypes = {
   pathname: PropTypes.string,
